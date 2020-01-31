15f4dec6ac758e4520ff32e7fbe1f0e679e334db
ross-u <cirkovic.uros.srb@gmail.com>
2020-01-31 17:42:58 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// STEP 1 - find object without indexing
+console.log(users);
+
+function findDocumentByIdWithoutIndex(collection, id, city) {
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i].id === id && collection[i].city === city) {
+      return collection[i];
+    }
+  }
+}
+
+console.time("Find user without index");
+
+var foundUser = findDocumentByIdWithoutIndex(users, 39999, "Los Angeles");
+
+console.timeEnd("Find user without index");
+
+console.log(foundUser);
+
+// STEP 2 - Index the users by city name
+function createIndex(data) {
+  const index = {};
+
+  data.forEach(function(userData) {
+    if (!index[userData.city]) {
+      index[userData.city] = [userData.id];
+    } else {
+      index[userData.city].push(userData.id);
+    }
+  });
+  return index;
+}
+
+console.time("Index the users by city");
+
+const indexedUsers = createIndex(users);
+
+console.timeEnd("Index the users by city");
+
+// INDEXED USERS
+console.log(indexedUsers);
+
+function findIndexedDocument(id, city) {
+  for (let i = 0; i < indexedUsers[city].length; i++) {
+    if (
+      indexedUsers[city][i].id === id &&
+      indexedUsers[city][i].city === city
+    ) {
+      return indexedUsers[city][i];
+    }
+  }
+}
+
+console.time("Find indexed user");
+
+var foundUser2 = findDocumentByIdWithoutIndex(39999, "Los Angeles");
+
+console.timeEnd("Find indexed user");
